de8a04b1180ac64e2ace14df374c5be909ab0ade
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-10-10 19:48:43 -0300

    Fix exercicio 3.

--- src/exercicio3/Contador.java
+++ src/exercicio3/Contador.java
@@ -1,7 +1,7 @@
 package exercicio3;
 
 public class Contador {
-    public int contarPalavras(String string){
+    public static int contarPalavras(String string){
         int numeroDePalavras = string.split(" ").length;
         System.out.println("A frase: \n\t\"" + string + "\" \ntem " + numeroDePalavras + " palavras.");
         return numeroDePalavras;
--- src/exercicio3/TestaContador.java
+++ src/exercicio3/TestaContador.java
@@ -2,7 +2,8 @@ package exercicio3;
 
 public class TestaContador {
     public static void main(String[] args) {
-        Contador contador = new Contador();
-        contador.contarPalavras("Agora temos um método contando!");
+        String frase = "Agora temos um método contando!";
+
+        Contador.contarPalavras(frase);
     }
 }
