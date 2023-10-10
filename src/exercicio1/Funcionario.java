27ea77428e8b7b997979d30773c28096f390f0bd
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-10-10 19:25:50 -0300

    exercicio1 terminado

--- src/exercicio1/Funcionario.java
+++ src/exercicio1/Funcionario.java
@@ -1,7 +1,24 @@
 package exercicio1;
 
 public class Funcionario {
-    private Long cpf;
-    private String nomeCompleto;
-    private double salario;
+
+    private String nome;
+    private float salario;
+
+    public Funcionario(){}
+    public Funcionario (String nome){
+        this();
+        this.nome = nome;
+    }
+    public Funcionario(String nome, float salario){
+        this(nome);
+        this.salario = salario;
+    }
+
+    public float aumentar(float valor){
+        return this.salario += valor;
+    }
+    public float aumentar(float valor, float comissao){
+        return this.salario += valor + comissao;
+    }
 }
